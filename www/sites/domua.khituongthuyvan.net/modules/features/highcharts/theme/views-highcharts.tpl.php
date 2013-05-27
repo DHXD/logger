8367d9b79e327cc45fdb040457deeecbbe44202e
tomphan <phanthaitrung@gmail.com>
2013-05-28 00:38:09 +0700

    12

--- www/sites/domua.khituongthuyvan.net/modules/features/highcharts/theme/views-highcharts.tpl.php
+++ www/sites/domua.khituongthuyvan.net/modules/features/highcharts/theme/views-highcharts.tpl.php
@@ -8,4 +8,9 @@
 
 
 ?>
+<style>
+div.highcharts-container svg g.highcharts-axis-labels{
+  display: none;
+}
+</style>
 <div class="views-highcharts-chart" id="<?php print($chart_id); ?>" style="margin: 0 auto"></div>
